ee410119ce9e50d35cf33d582f4d8eca4c3d60f8
Don-Harice <don.harice78.com>
2024-08-23 01:54:43 +0200

    portfolio-fr

--- script.js
+++ script.js
@@ -33,4 +33,16 @@ window.onscroll = () => {
 
 }
 
+ScrollReveal({
+    
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, portfolio-content, .contact form', { origin: 'bottom' }); 
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
 
